63690d326ef1af2eb0128642a013756c68ad046f
Miguel Franco <francamente@hotmail.es>
2023-12-19 22:47:35 +0100

    post /login

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -3,4 +3,22 @@ const getStart = (req, res) => {
     res.send(respuesta)
 }
 
-module.exports = {getStart}
+const loginUser = async (req, res) => {
+    try{
+        let params = [req.body.email, req.body.password]
+        let sql = "SELECT id_user, email, name, surname, location, phonenumber, photo, company FROM hirun.user WHERE email = ? AND password = ?"
+        let [result] = await pool.query(sql, params)
+        let resultado = result[0]
+        let respuesta
+        if(result.length > 0){
+            respuesta = {error: false, codigo: 200, message: "Datos correctos", data: resultado}
+        }else{
+            respuesta = {error: true, codigo: 200, message: "Datos incorrectos"}
+        }
+        res.send(respuesta)
+    }catch(error){
+        console.log(error)
+    }
+}
+
+module.exports = {getStart, loginUser}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,4 +4,6 @@ const userCtrl = require('../controller/user.controller')
 
 router.get("/", userCtrl.getStart)
 
+router.post("/login", userCtrl.loginUser)
+
 module.exports = router
