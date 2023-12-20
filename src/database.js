4237edcd552e8d299ae2a99e2f3f2b7b209a7a4e
ZuriGE <zurigales@gmail.com>
2023-12-20 08:50:50 +0100

    undo changes to connection info

--- src/database.js
+++ src/database.js
@@ -1,25 +1,24 @@
 const mysql = require("mysql2");
 const ddbbConfig = require("../ddbbConfig");
 
-//dejo comentado por si no os funciona
-// const connection = mysql
-// 	.createConnection({
-// 		host: process.env.DB_HOST || "hirun.cxnp8cr5xgbf.eu-north-1.rds.amazonaws.com",
-// 		user: process.env.DB_USER || "hirun_admin",
-// 		password: process.env.DB_PASSWORD || "dYrW59fz4<0",
-// 		database: process.env.DB_DATABASE || "hirun",
-// 		port: process.env.DB_PORT || 3306,
-// 	})
-// 	.promise();
-
 const connection = mysql
 	.createConnection({
-		host: ddbbConfig.host,
-		user: ddbbConfig.user,
-		password: ddbbConfig.password,
-		database: ddbbConfig.database,
+		host: process.env.DB_HOST || "hirun.cxnp8cr5xgbf.eu-north-1.rds.amazonaws.com",
+		user: process.env.DB_USER || "hirun_admin",
+		password: process.env.DB_PASSWORD || "dYrW59fz4<0",
+		database: process.env.DB_DATABASE || "hirun",
 		port: process.env.DB_PORT || 3306,
 	})
 	.promise();
+//dejo comentado por si no os funciona
+// const connection = mysql
+// 	.createConnection({
+// 		host: ddbbConfig.host,
+// 		user: ddbbConfig.user,
+// 		password: ddbbConfig.password,
+// 		database: ddbbConfig.database,
+// 		port: process.env.DB_PORT || 3306,
+// 	})
+// 	.promise();
 
 module.exports = { connection };
