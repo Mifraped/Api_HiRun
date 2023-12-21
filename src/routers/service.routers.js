136397377299f17683fa2a0463661193cdddf5ad
Miguel Franco <francamente@hotmail.es>
2023-12-21 12:09:17 +0100

    añago getService

--- src/routers/service.routers.js
+++ src/routers/service.routers.js
@@ -4,4 +4,6 @@ const serviceCtrl = require("../controller/service.controller");
 
 router.post("/service", serviceCtrl.postService);
 
+router.get("/service", serviceCtrl.getService)
+
 module.exports = router;
