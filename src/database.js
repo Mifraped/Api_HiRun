98b2e8d69541e921c2f632c7b50da4ef6933052f
Miguel Franco <francamente@hotmail.es>
2024-01-10 10:59:46 +0100

    variables de entorno

--- src/database.js
+++ src/database.js
@@ -6,8 +6,8 @@ const { S3Client } = require("@aws-sdk/client-s3");
 const s3Client = new S3Client({
   region: "eu-west-3",
   credentials: {
-    accessKeyId: "AKIA3FZLB74P2IONL6Y5",
-    secretAccessKey: "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",
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   },
 });
 
@@ -16,7 +16,7 @@ const pool = mysql
     host:
       process.env.DB_HOST || "hirun.cxnp8cr5xgbf.eu-north-1.rds.amazonaws.com",
     user: process.env.DB_USER || "hirun_admin",
-    password: process.env.DB_PASSWORD || "2P1^u3h0go",
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_DATABASE || "hirun",
     port: process.env.DB_PORT || 3306,
     multipleStatements: true
