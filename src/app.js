03aafed8331ce4c9ca740291ffea92f36904d1b5
Charlatan2k <octavianmihai.calin@gmail.com>
2024-01-12 12:11:56 +0100

    Fix cors

--- src/app.js
+++ src/app.js
@@ -24,22 +24,22 @@ const path = require("path");
 
 app.set("port", process.env.PORT || 3000);
 
-app.use(cors(['https://hi-run.vercel.app/', 'http://localhost:4200/']));
+app.use(cors((origin = "*")));
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.get("/get-image/:imageName", (req, res) => {
-	try {
-		const imageName = req.params.imageName;
-		const imagePath = path.join(__dirname, "uploads", imageName);
+  try {
+    const imageName = req.params.imageName;
+    const imagePath = path.join(__dirname, "uploads", imageName);
 
-		// Envía la imagen como respuesta
-		res.sendFile(imagePath);
-	} catch (error) {
-		console.error(error);
-		res.status(500).send("Error interno del servidor");
-	}
+    // Envía la imagen como respuesta
+    res.sendFile(imagePath);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error interno del servidor");
+  }
 });
 
 app.use(errorHandling);
@@ -60,11 +60,11 @@ app.use(preferencesRouter);
 app.use(ratingRouter);
 
 app.use(function (req, res, next) {
-	res.status(404).json({
-		error: true,
-		codigo: 404,
-		message: "Endpoint doesnt found",
-	});
+  res.status(404).json({
+    error: true,
+    codigo: 404,
+    message: "Endpoint doesnt found",
+  });
 });
 
 module.exports = app;
