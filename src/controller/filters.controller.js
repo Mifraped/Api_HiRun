65af669b749dbdfa1c986cd177e6a206d1687819
Charlatan2k <octavianmihai.calin@gmail.com>
2024-01-12 12:46:41 +0100

    Fix filter rating y cat

--- src/controller/filters.controller.js
+++ src/controller/filters.controller.js
@@ -1,49 +1,49 @@
-const mysql2 = require('mysql2');
+const mysql2 = require("mysql2");
 
-const { pool } = require('../database');
+const { pool } = require("../database");
 
 const testDbConnection = (req, res) => {
   pool
-    .query('SELECT 1 + 1 AS solution')
+    .query("SELECT 1 + 1 AS solution")
     .then(([results, fields]) => {
-      res.send('The solution is: ' + results[0].solution);
+      res.send("The solution is: " + results[0].solution);
     })
     .catch((error) => {
-      console.log('Error querying the database', error);
-      res.status(500).send('Error querying the database');
+      console.log("Error querying the database", error);
+      res.status(500).send("Error querying the database");
     });
 };
 
 const getBestRated = (req, res) => {
   pool
     .execute(
-      'SELECT business.*, users.name AS providerName, users.surname AS providerSurname, users.photo AS userPhoto, service.price, service.description FROM business JOIN users ON business.provider = users.id_user JOIN service ON business.id_business = service.id_business WHERE business.rating >= 4'
+      "SELECT business.*, users.name AS providerName, users.surname AS providerSurname, users.photo AS userPhoto, service.price, service.description FROM business JOIN users ON business.provider = users.id_user JOIN service ON business.id_business = service.id_business WHERE business.rating >= 4"
     )
     .then(([results, fields]) => {
       res.json(results);
     })
     .catch((error) => {
-      console.log('Error querying the database', error);
-      res.status(500).send('Error querying the database');
+      console.log("Error querying the database", error);
+      res.status(500).send("Error querying the database");
     });
 };
 
 const getNovedades = (req, res) => {
   pool
     .execute(
-      'SELECT business.*, users.name AS providerName, users.surname AS providerSurname, users.photo AS userPhoto, service.price, service.description FROM business JOIN users ON business.provider = users.id_user JOIN service ON business.id_business = service.id_business ORDER BY business.create_date DESC LIMIT 10'
+      "SELECT business.*, users.name AS providerName, users.surname AS providerSurname, users.photo AS userPhoto, service.price, service.description FROM business JOIN users ON business.provider = users.id_user JOIN service ON business.id_business = service.id_business ORDER BY business.create_date DESC LIMIT 10"
     )
     .then(([results, fields]) => {
       res.json(results);
     })
     .catch((error) => {
-      console.log('Error querying the database', error);
-      res.status(500).send('Error querying the database');
+      console.log("Error querying the database", error);
+      res.status(500).send("Error querying the database");
     });
 };
 
 const getResults = (req, res) => {
-  const searchTerm = req.query.searchTerm || '';
+  const searchTerm = req.query.searchTerm || "";
   const rating = req.query.ratingFilter;
   const minPrice = req.query.minPrice;
   const maxPrice = req.query.maxPrice;
@@ -65,7 +65,7 @@ FROM
   business
   JOIN users ON business.provider = users.id_user
   JOIN service ON business.id_business = service.id_business
-  JOIN business_cat ON business.id_business = business_cat.business
+  LEFT JOIN business_cat ON business.id_business = business_cat.business
   JOIN category ON business_cat.category = category.id_category
   LEFT JOIN business_options ON business.id_business = business_options.business
   LEFT JOIN options ON business_options.id_options = options.id_options
@@ -73,14 +73,18 @@ WHERE 1=1`;
 
   let queryParams = [];
 
-  if (searchTerm.trim() !== '') {
+  if (searchTerm.trim() !== "") {
     query += ` AND business.title LIKE ?`;
     queryParams.push(`%${searchTerm}%`);
   }
 
   if (rating) {
-    query += ` AND business.rating = ?`;
-    queryParams.push(rating);
+    const ratingNumber = parseFloat(rating);
+    query += ` AND business.rating >= ? AND business.rating < ?`;
+    queryParams.push(
+      ratingNumber.toString(),
+      (Math.floor(ratingNumber) + 1 - 0.01).toString()
+    );
   }
 
   if (minPrice) {
@@ -99,33 +103,33 @@ WHERE 1=1`;
   }
 
   if (otherValues) {
-    if (typeof otherValues === 'string') {
+    if (typeof otherValues === "string") {
       otherValues = [otherValues]; // If 'other' is a string, convert it to an array
     }
 
-    query += ' AND (';
+    query += " AND (";
     otherValues.forEach((value, index) => {
       if (index > 0) {
-        query += ' OR ';
+        query += " OR ";
       }
       query += `options.title = ?`; // Add a condition for each option
       queryParams.push(value); // Add the option to the query parameters
     });
-    query += ')';
+    query += ")";
   }
 
   query += ` GROUP BY business.id_business`;
 
   if (orderBy) {
     switch (orderBy) {
-      case 'Mejores Valorados':
-        orderBy = 'rating';
+      case "Mejores Valorados":
+        orderBy = "rating";
         break;
-      case 'Más baratos':
-        orderBy = 'price';
+      case "Más baratos":
+        orderBy = "price";
         break;
-      case 'Recientes':
-        orderBy = 'create_date';
+      case "Recientes":
+        orderBy = "create_date";
         break;
     }
     query += ` ORDER BY ${orderBy}`;
@@ -133,8 +137,8 @@ WHERE 1=1`;
 
   console.log(req.query);
 
-  console.log('Executing query:', query);
-  console.log('Query parameters:', queryParams);
+  console.log("Executing query:", query);
+  console.log("Query parameters:", queryParams);
 
   pool
     .execute(query, queryParams)
@@ -143,8 +147,8 @@ WHERE 1=1`;
     })
     .catch((error) => {
       console.log(error);
-      console.log('Error querying the database', error);
-      res.status(500).send('Error querying the database');
+      console.log("Error querying the database", error);
+      res.status(500).send("Error querying the database");
     });
 };
 
